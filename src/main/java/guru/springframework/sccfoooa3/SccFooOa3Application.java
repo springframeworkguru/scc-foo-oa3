cc47209999cf4a54a27bd8b090a8d30c87313900
John Thompson <john@springframework.guru>
2018-07-19 15:59:33 -0400

    Initial commit

---
+++ src/main/java/guru/springframework/sccfoooa3/SccFooOa3Application.java
@@ -0,0 +1,12 @@
+package guru.springframework.sccfoooa3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SccFooOa3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SccFooOa3Application.class, args);
+	}
+}
